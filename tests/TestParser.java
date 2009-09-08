3a1b8646e06c11a60222af3d6ec0c99906d8e74d
Essien Ita Essien <essiene@gmail.com>
2009-09-08 20:39:51 +0100

    Start unittesting parser
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ tests/TestParser.java
@@ -0,0 +1,53 @@
+import junit.framework.*;
+
+import java.util.*;
+
+import dit.url.*;
+import dit.url.ast.*;
+import dit.url.parser.*;
+
+
+public class TestParser extends TestCase
+{
+    private Parser p;
+
+    public TestParser(String s)
+    {
+        super(s);
+    }
+
+    public void setUp()
+    {
+        p = new Parser(System.in);
+    }
+
+    public void testSingleText() throws ParseException
+    {
+        p.ReInit("one");
+
+        List<Ast> l = p.UrlDeclaration();
+
+        assertEquals(1, l.size());
+        assertEquals("[AST{value:one}]", l.toString());
+    }
+
+    public void testSingleUntypedVariable() throws ParseException
+    {
+        p.ReInit("$one");
+
+        List<Ast> l = p.UrlDeclaration();
+
+        assertEquals(1, l.size());
+        assertEquals("[AST{name:one,type:class java.lang.String}]", l.toString());
+    }
+
+    public void testSingleTypedVariable() throws ParseException
+    {
+        p.ReInit("$one:Boolean");
+
+        List<Ast> l = p.UrlDeclaration();
+
+        assertEquals(1, l.size());
+        assertEquals("[AST{name:one,type:class java.lang.Boolean}]", l.toString());
+    }
+}
