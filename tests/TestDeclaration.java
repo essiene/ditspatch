4a4be228bb72e80391af19cc14be109daa3ecf49
Essien Ita Essien <essiene@gmail.com>
2009-09-08 18:25:51 +0100

    Test Declaration.toPattern conversions
    Everything works ok
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- tests/TestDeclaration.java
+++ tests/TestDeclaration.java
@@ -1,5 +1,6 @@
 import junit.framework.*;
 
+import java.util.regex.*;
 
 import dit.url.*;
 
@@ -30,6 +31,20 @@ public class TestDeclaration extends TestCase
         assertEquals("one/two/three", d.getPattern());
     }
 
+    public void testVariablePatternRegexp()
+    {
+        TestDeclaration.checkPattern(Integer.class, "123");
+        TestDeclaration.checkPattern(Character.class, "a");
+        TestDeclaration.checkPattern(String.class, "abc");
+        TestDeclaration.checkPattern(Boolean.class, "true");
+        TestDeclaration.checkPattern(Boolean.class, "false");
+    }
 
+    public static void checkPattern(Class c, String s)
+    {
+        Pattern p = Pattern.compile(Declaration.toPattern(c));
+        Matcher m = p.matcher(s);
+        assertTrue(m.matches());
+        assertEquals(1, m.groupCount());
+    }
 }
-
