2f966d7793fa87650f9dde0bc02c371fc4f23c69
Essien Ita Essien <essiene@gmail.com>
2009-09-08 19:51:25 +0100

    Implement Ast for distpatch variables
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/dit/url/ast/Variable.java
@@ -0,0 +1,53 @@
+package dit.url.ast;
+
+import dit.url.*;
+
+public class Variable implements Ast
+{
+    private String name;
+    private Class type;
+
+    public Variable(String name, String type)
+    {
+        this.name = name;
+        
+        type = type.trim();
+        if(type.equals("Integer")) {
+            this.type = Integer.class;
+            return;
+        } 
+
+        if(type.equals("String")) {
+            this.type = String.class;
+            return;
+        }
+
+        if(type.equals("Character")) {
+            this.type = Character.class;
+            return;
+        }
+
+        if(type.equals("Boolean")) {
+            this.type = Boolean.class;
+            return;
+        }
+
+        throw new IllegalArgumentException("Type is unsupported by Ditspatch: " + type);
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public Class getType()
+    {
+        return this.type;
+    }
+
+    public Declaration process(Declaration d)
+    {
+        d.addVariable(name, type);
+        return d;
+    }
+}
